e2f218e1e19666ec5315177931b2d2081836b651
barrymulinwqn <xueegdlut@163.com>
2026-01-01 16:07:38 +0800

    enableHighlightFloatingButton default value is true

--- web/app_options.js
+++ web/app_options.js
@@ -246,7 +246,8 @@ const defaultOptions = {
     // in Firefox release, but it has to be temporary.
     // TODO: remove it when unnecessary.
     /** @type {boolean} */
-    value: typeof PDFJSDev === "undefined" || PDFJSDev.test("TESTING"),
+    // value: typeof PDFJSDev === "undefined" || PDFJSDev.test("TESTING"),
+    value: true,
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE,
   },
   enableNewAltTextWhenAddingImage: {
